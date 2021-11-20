4bba49461a7693dcf7cf0a80981c6183f7b0f2d7
Jonas <i.jonas.hack.i@googlemail.com>
2021-11-21 00:25:40 +0100

    Graph properly shows intervals + padding

--- SusOp/src/UI/LineGraph.java
+++ SusOp/src/UI/LineGraph.java
@@ -11,6 +11,7 @@ import static java.util.Comparator.*;
 public class LineGraph extends JPanel
 {
     private LinkedHashMap<Integer, Integer> data = new LinkedHashMap<Integer, Integer>();
+    int padding = 8;
 
     public LineGraph()
     {
@@ -31,33 +32,34 @@ public class LineGraph extends JPanel
         int min = data.values().stream().min(Comparator.<Integer>naturalOrder()).get();
         int max = data.values().stream().max(Comparator.<Integer>naturalOrder()).get();
 
-        double xMul = (double) getWidth() / (double) data.keySet().stream().max(Comparator.<Integer>naturalOrder()).get();
-        double yMul = (double) getHeight() / (double) Math.abs(min - max);
+        double xMul = (double) (getWidth()-padding*2) / (double) data.keySet().stream().max(Comparator.<Integer>naturalOrder()).get();
+        double yMul = (double) (getHeight()-padding*2) / (double) Math.abs(min - max);
 
-
+        g2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
         g2D.setStroke(new BasicStroke(2f));
 
         for(int i = 0; i < (max-min); i += 5)
         {
-            int y = getHeight () - (int) (i * yMul);
+            int y = getHeight () - (int) (i * yMul) - padding;
 
             g2D.setColor(Style.backgroundColor);
-            g2D.drawLine(0, y, getWidth(), y);
+            g2D.drawLine(padding, y, getWidth()-padding, y);
 
             g2D.setColor(Style.textColor);
-            g2D.drawString("" + i, 8, y + 2);
+            g2D.drawString("" + i, 8+padding, y);
         }
 
-        for(int i = 0; i < data.size(); i += 30)
+        for(int i = 0; i < data.keySet().stream().max(Comparator.<Integer>naturalOrder()).get(); i+= 30)
         {
-            int x = (int) (i * xMul);
+            int x = (int) (i * xMul) + padding;
 
             g2D.setColor(Style.backgroundColor);
-            g2D.drawLine(x, 0, x, getHeight());
+            g2D.drawLine(x, padding, x, getHeight()-padding);
 
             //g2D.setColor(Style.textColor);
             //g2D.drawString("" + i, 8, y + 2);
+
         }
 
 
@@ -65,8 +67,8 @@ public class LineGraph extends JPanel
 
         for (int key : data.keySet())
         {
-            int x = (int) (key * xMul);
-            int y = getHeight() - (int) (data.get(key) * yMul);
+            int x = (int) (key * xMul) + padding;
+            int y = getHeight() - (int) ((double)data.get(key) * yMul) - padding;
 
             points.add(new Point(x, y));
         }
